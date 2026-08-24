904e124a59a9be3aba53d12329d08ea6d5b889b0
ARAVINDRASAKATLA <aravindrasakatla7@gmail.com>
2026-08-24 15:35:19 +0530

    Remove cart implementation

--- src/main/java/com/example/project/cart/repository/CartItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.project.cart.repository;
-
-import com.example.project.cart.entity.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
-}
